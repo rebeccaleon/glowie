20bb8b4110d1db10572576331d313e63068fd053
Rebecca Leon <rebecca.leonx@gmail.com>
2018-10-08 18:57:21 -0400

    Functional Quiz

---
+++ client/src/api/quizQuestions.js
@@ -0,0 +1,109 @@
+var quizQuestions = [
+    {
+        question: "Question One",
+        answers: [
+            {
+                type: "A",
+                content: "Option A"
+            },
+            {
+                type: "B",
+                content: "Option B"
+            },
+            {
+                type: "C",
+                content: "Option C"
+            },
+            {
+                type: "D",
+                content: "Option D"
+            }
+        ]
+    },
+    {
+        question: "Question Two",
+        answers: [
+            {
+                type: "A",
+                content: "Option A"
+            },
+            {
+                type: "B",
+                content: "Option B"
+            },
+            {
+                type: "C",
+                content: "Option C"
+            },
+            {
+                type: "D",
+                content: "Option D"
+            }
+        ]
+    },
+    {
+        question: "Question Three",
+        answers: [
+            {
+                type: "A",
+                content: "Option A"
+            },
+            {
+                type: "B",
+                content: "Option B"
+            },
+            {
+                type: "C",
+                content: "Option C"
+            },
+            {
+                type: "D",
+                content: "Option D"
+            }
+        ]
+    },
+    {
+        question: "Question Four",
+        answers: [
+            {
+                type: "A",
+                content: "Option A"
+            },
+            {
+                type: "B",
+                content: "Option B"
+            },
+            {
+                type: "C",
+                content: "Option C"
+            },
+            {
+                type: "D",
+                content: "Option D"
+            }
+        ]
+    },
+    {
+        question: "Question Five",
+        answers: [
+            {
+                type: "A",
+                content: "Option A"
+            },
+            {
+                type: "B",
+                content: "Option B"
+            },
+            {
+                type: "C",
+                content: "Option C"
+            },
+            {
+                type: "D",
+                content: "Option D"
+            }
+        ]
+    }
+  ];
+  
+  export default quizQuestions;
